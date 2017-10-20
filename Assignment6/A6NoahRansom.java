a1f27ab9724798fe7238cefb691f6255ae698c96
postmodern-cereal <noah.ransom@maine.edu>
2017-10-20 12:02:41 -0400

    Completed Assignment 6

--- Assignment6/A6NoahRansom.java
+++ Assignment6/A6NoahRansom.java
@@ -1,12 +1,15 @@
-
+//By Noah Ransom
+//Reads studen IDs and number grades from a specified file, then prints letter grades and analysis
 
 import java.io.*;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 class A6NoahRansom
 {
     public static int numRecords(String fileName) throws IOException
     {
+        //POST: return how many records are in the file
         Scanner reader = new Scanner(new FileReader(fileName));
         int toReturn = reader.nextInt();
         reader.close();
@@ -16,6 +19,7 @@ class A6NoahRansom
 
     public static void populate(String[] ids, int[] midGrades, int[] finGrades, String fileName) throws IOException
     {
+                                                                //POST: populate the arrays with data from the file
         Scanner reader = new Scanner(new FileReader(fileName)); //Reads the file
         reader.nextLine();                                      //Skip the irrelevant first line telling us how many records there are
         String currLine;                                        //Holds current line
@@ -47,7 +51,7 @@ class A6NoahRansom
         return sum/data.length;                 //perform the division operation and return
     }
 
-    public static double standardDeviation(int[] data. double mean)
+    public static double standardDeviation(int[] data, double mean)
     {
         //POST: return the standard deviation according to the formula posted online
         double sum = 0;
@@ -89,8 +93,8 @@ class A6NoahRansom
     public static void showHistogram(int[] counts)
     {
         //POST: Prints the quantities of each letter grade as a histogram
-        char leter = '';                //holds the current letter
-        for(int i = 0; i < 6; i ++)    //even if the array has more than 5 elements, there are only 5 letter grades to give
+        char letter;                //holds the current letter
+        for(int i = 0; i < 5; i ++)    //even if the array has more than 5 elements, there are only 5 letter grades to give
         {
             letter = (char)(i + 65);  //turns i into a capital letter
             System.out.print(letter + "\t");    //print out the letter and a blank space
@@ -104,9 +108,12 @@ class A6NoahRansom
 
     public static void main(String[] args)
     {
+        DecimalFormat fmt = new DecimalFormat("#.#");
         Scanner scan = new Scanner(System.in);
         String fileName = "";
         int records = -1;
+        int[] midCounts = new int[5];           //holds a count of each letter grade for midterm exam
+        int[] finCounts = new int[5];           //holds a count of each letter grade for final exam
         String[] ids;                           //student ids
         int[] midGrades;                        //midterm number grades
         int[] finGrades;                        //final number grades
@@ -115,9 +122,10 @@ class A6NoahRansom
         double meanFin = 0;                     //final mean
         double stdDevFin = 0;                   //final standard deviation
 
-        System.out.println("Grade roster by Noah Ransom");
+        System.out.println("Grade roster by Noah Ransom\n");
 
         //The loop below ensures that we get a valid file name
+        //if an invalid file name is entered, we start over
         while(records < 0)
         {
             System.out.print("Enter name of file: ");
@@ -132,6 +140,8 @@ class A6NoahRansom
             }
         }
         scan.close();
+        System.out.println();
+
         ids = new String[records];
         midGrades = new int[records];
         finGrades = new int[records];
@@ -141,8 +151,8 @@ class A6NoahRansom
             populate(ids, midGrades, finGrades, fileName);
         }catch(IOException e){}
 
-        meanMt = mean(midGrades);
-        stdDevMT = standardDeviation(midGrades, meanMt);
+        meanMT = mean(midGrades);
+        stdDevMT = standardDeviation(midGrades, meanMT);
 
         meanFin = mean(finGrades);
         stdDevFin = standardDeviation(finGrades, meanFin);
@@ -153,13 +163,66 @@ class A6NoahRansom
         int mtWid = 7 + buffer;
         int gradeWid = 5 + buffer;      //same width for both midterm and final, because both have same title
         int finWid = 5 + buffer;
-
-        //TODO: write format strings for the titles and the data
-        //TODO: print the data to the console
-        //TODO: format mean/stdDev to one decimal place and print to console
-        //TODO: print histograms to console
+        int totWid = studWid + mtWid + finWid + gradeWid + gradeWid - buffer;    //holds total width of output. used for dashes. gradewid appears twice because there are two grade columns
+        //I subtract the buffer width from the above to account for the fact that there is no buffer needed after the last column
 
 
+        String fmtStud = "%-" + studWid + "s";              //The format strings are designed to print the actual data entries
+        String fmtMT = "%-" + mtWid + "d";                  //I manually created the strings for the column titles because this was easier than
+        String fmtGrade = "%-" + gradeWid + "c";            //making two seperate sets of format strings
+        String fmtFin = "%-" + finWid + "d";
 
+
+        //Print titles
+        System.out.print(String.format(fmtStud, "Student"));
+        System.out.print(String.format(("%-" + mtWid + "s"), "Midterm"));
+        System.out.print(String.format(("%-" + gradeWid + "s"), "Grade"));
+        System.out.print(String.format(("%-" + finWid + "s"), "Final"));
+        System.out.print(String.format(("%-" + gradeWid + "s"), "Grade"));
+        System.out.println();
+        //Print dashes
+        for(int i = 0; i < totWid; i++)
+        {
+            System.out.print("-");
+        }
+        System.out.println();
+
+
+        //print and format the student records
+        char midLetter;        //temporary variable to store letter grade for miterm
+        char finLetter;          //temproary variable to store letter grade for final
+        for(int i = 0; i < records; i++)
+        {
+            midLetter = letterGrade(midGrades[i], meanMT, stdDevMT);
+            finLetter = letterGrade(finGrades[i], meanFin, stdDevFin);
+            //Increment letter grade counts
+            midCounts[(int)(midLetter - 65)]++;
+            finCounts[(int)(finLetter - 65)]++;
+            System.out.print(String.format(fmtStud, ids[i]));
+            System.out.print(String.format(fmtMT, midGrades[i]));
+            System.out.print(String.format(fmtGrade, midLetter));
+            System.out.print(String.format(fmtFin, finGrades[i]));
+            System.out.print(String.format(fmtGrade, finLetter));
+            System.out.println();
+
+        }
+        System.out.println();
+
+        //print midterm analysis
+        System.out.println("Midterm Analysis");
+        System.out.printf("%-10s", "Mean:");          //I'm using a field width of 10 because the std dev: line is 8 characters long plus 2 characters of buffer
+        System.out.printf("%-3s\n", fmt.format(meanMT));
+        System.out.printf("%-10s", "Std Dev:");
+        System.out.printf("%-3s\n", fmt.format(stdDevMT));
+        showHistogram(midCounts);
+        System.out.println();
+
+        //print final analysis
+        System.out.println("Final Analysis");
+        System.out.printf("%-10s", "Mean:");          //I'm using a field width of 10 because the std dev: line is 8 characters long plus 2 characters of buffer
+        System.out.printf("%-3s\n", fmt.format(meanFin));
+        System.out.printf("%-10s", "Std Dev:");
+        System.out.printf("%-3s\n", fmt.format(stdDevFin));
+        showHistogram(finCounts);
     }
 }
